c37dd439933525db09660773776573e319e95e70
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-13 20:35:37 -0500

    commit tree bd0a28fb440230d09a6e32c3ffc5f92431202541

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
 
+class CashRegister
+
+   attr_accessor :total, :discount
+
+    def initialize(total= 0)
+        @total = total
+        @discount = 20  
+    end 
+
+    def add_item(title, price, quantity)
+        @quantity = quantity
+        @title = title
+        @price = price
+        self.total += @price
+    end 
+end 
